0de464cc337264770fc5f7c1429f86567f0a5dff
Juan Miguel Garcia <juan.miguel@f.zetaol.com>
2022-12-19 16:33:36 +0800

    update function on school almost done

--- js/validation.js
+++ js/validation.js
@@ -1,63 +1,26 @@
     const schoolModalButton = document.getElementById("school_submit_btn");
+    const school_id_input = document.getElementById("school_id");
     const school_name_input = document.getElementById("school_name");
     const school_address_input = document.getElementById("school_address");
    
    /* onLogin function */ 
-   function onLogin() {
-        var username = $('#username').val();  
-        var password = $('#password').val();  
-        
-        if(username == '' || password == ''){  
-            alert('All Fields are required!');
-        }else{
-            $.ajax({  
-                url:"../php/login.php",  
-                method:"POST",  
-                data: $('#submit_form').serialize(),  
-                success: function(response) {
-                    var jsonData = JSON.parse(response);
-                    if (jsonData.success){
-                        alert("Successfully login ");
-                        sessionStorage.setItem("account",response);
-                        location.href = '../admin/pages/dashboard.html';
-                    }else{
-                        alert(jsonData.error_msg);
-                    }
-                },
-                error: function() {
-                    alert('System error: Ajax not working properly');
-                }  
-            }); 
-        }
-   }
-    function onViewSchool() {
+function onLogin() {
+    var username = $('#username').val();  
+    var password = $('#password').val();  
+    
+    if(username == '' || password == ''){  
+        alert('All Fields are required!');
+    }else{
         $.ajax({  
-            url:"../../php/onviewschool.php",  
-            method:"GET",  
-            data: "",  
+            url:"../php/login.php",  
+            method:"POST",  
+            data: $('#submit_form').serialize(),  
             success: function(response) {
                 var jsonData = JSON.parse(response);
-                console.log(jsonData.data);
                 if (jsonData.success){
-                    sessionStorage.setItem("school_list",response);
-                    var table = document.querySelector("table");
-                    var template;
-                    var ctr=0;
-                    jsonData.data.forEach(element => {
-                          ctr = ctr + 1;
-                          template = 
-                        `<tr>
-                            <td>${ctr}</td>
-                            <td>${element.id}</td>
-                            <td>${element.school_name}</td>
-                            <td>${element.school_address}</td>
-                            <td>
-                                <span  data-bs-toggle="modal" data-bs-target="#schooModal" class="action-button" onClick="onEditSchool(${element.id})" >Edit</span> | <span class="action-button">Delete</span> 
-                            </td>
-                        </tr>`;
-                        table.innerHTML += template;
-                    });
-                   
+                    alert("Successfully login ");
+                    sessionStorage.setItem("account",response);
+                    location.href = '../admin/pages/dashboard.html';
                 }else{
                     alert(jsonData.error_msg);
                 }
@@ -66,49 +29,108 @@
                 alert('System error: Ajax not working properly');
             }  
         }); 
-        
     }
-
-    function onAddSchoolModal() {
-        schoolModalButton.innerText  = "Submit";
-        school_name_input.value = "";
-        school_address_input.innerText = "";
-    }
-    function onAddSchool() {
-        var school_name = $('#school_name').val();  
-        var school_address = $('#school_address').val();  
-        
-        if(school_name == '' || school_address == ''){  
-            alert('All Fields are required!');
-        }else{
-            $.ajax({  
-                url:"../../php/onaddschool.php",  
-                method:"POST",  
-                data: $('#school_form').serialize(),  
-                success: function(response) {
-                    var jsonData = JSON.parse(response);
-                    if (jsonData.success){
-                        alert(jsonData.success_msg);
-                        location.href = '../pages/school.html';
-                        onViewSchool();
-                    }else{
-                        alert(jsonData.error_msg);
-                    }
-                  },
-                  error: function() {
-                    alert('System error: Ajax not working properly');
-                  }  
-            }); 
-        }
-    }
-    function onEditSchool(id) {
-        let school_list = sessionStorage.getItem("school_list");
-        var jsonData = JSON.parse(school_list);
-        schoolModalButton.innerText  = "Update";
-        jsonData.data.forEach(element => {
-            if(element.id == id){
-                school_name_input.value = element.school_name;
-                school_address_input.innerText = element.school_address;
+}
+function onViewSchool() {
+    $.ajax({  
+        url:"../../php/onviewschool.php",  
+        method:"GET",  
+        data: "",  
+        success: function(response) {
+            var jsonData = JSON.parse(response);
+            console.log(jsonData.data);
+            if (jsonData.success){
+                sessionStorage.setItem("school_list",response);
+                var table = document.querySelector("table");
+                var template;
+                var ctr=0;
+                jsonData.data.forEach(element => {
+                        ctr = ctr + 1;
+                        template = 
+                    `<tr>
+                        <td>${ctr}</td>
+                        <td>${element.id}</td>
+                        <td>${element.school_name}</td>
+                        <td>${element.school_address}</td>
+                        <td>
+                            <span  data-bs-toggle="modal" data-bs-target="#schooModal" class="action-button" onClick="onClickEditSchool(${element.id})" >Edit</span> | <span class="action-button">Delete</span> 
+                        </td>
+                    </tr>`;
+                    table.innerHTML += template;
+                });
+                
+            }else{
+                alert(jsonData.error_msg);
             }
-        });
+        },
+        error: function() {
+            alert('System error: Ajax not working properly');
+        }  
+    }); 
+    
+}
+function onClickAddSchoolModal() {
+    schoolModalButton.innerText  = "Submit";
+    school_name_input.value = "";
+    school_address_input.innerText = "";
+}
+function onAddSchool() {
+    if(school_name_input.val() == '' || school_address_input.val() == ''){  
+        alert('All Fields are required!');
+    }else{
+        $.ajax({  
+            url:"../../php/onaddschool.php",  
+            method:"POST",  
+            data: $('#school_form').serialize(),  
+            success: function(response) {
+                var jsonData = JSON.parse(response);
+                if (jsonData.success){
+                    alert(jsonData.success_msg);
+                    location.href = '../pages/school.html';
+                    onViewSchool();
+                }else{
+                    alert(jsonData.error_msg);
+                }
+                },
+                error: function() {
+                alert('System error: Ajax not working properly');
+                }  
+        }); 
     }
+}   
+function onClickEditSchool(id) {
+    let school_list = sessionStorage.getItem("school_list");
+    var jsonData = JSON.parse(school_list);
+    schoolModalButton.innerText  = "Update";
+    jsonData.data.forEach(element => {
+        if(element.id == id){
+            school_name_input.value = element.school_name;
+            school_address_input.innerText = element.school_address;
+            school_id_input.value = element.id
+        }
+    });
+}
+function onUpdateSchool() {
+    if(school_name_input.val() == '' || school_address_input.val() == ''){  
+        alert('All Fields are required!');
+    }else{
+        $.ajax({  
+            url:"../../php/onupdateschool.php?id=2",  
+            method:"POST",  
+            data: $('#school_form').serialize(),  
+            success: function(response) {
+                var jsonData = JSON.parse(response);
+                if (jsonData.success){
+                    alert(jsonData.success_msg);
+                    location.href = '../pages/school.html';
+                    onViewSchool();
+                }else{
+                    alert(jsonData.error_msg);
+                }
+                },
+                error: function() {
+                alert('System error: Ajax not working properly');
+                }  
+        }); 
+    }
+}
--- php/onupdateschool.php
+++ php/onupdateschool.php
@@ -3,13 +3,21 @@ include("connection.php");
 
         $school_name=$_POST['school_name'];
         $school_address=$_POST['school_address'];
+        $school_id=$_POST['school_id'];
+
         
         $school_name = stripslashes($school_name);
         $school_address = stripslashes($school_address);
         $school_name = mysqli_real_escape_string($db, $school_name);
         $school_address = mysqli_real_escape_string($db, $school_address);
+
+
+       /*  UPDATE MyGuests SET lastname='Doe' WHERE id=2 */
+
+       echo '<script> alert('$school_id') </script>';
         
-        $sql=("INSERT INTO tbl_school (school_name,school_address) VALUES ('$school_name','$school_address')");
+       /*  school_id
+        $sql=("UPDATE tbl_school  (school_name,school_address) VALUES ('$school_name','$school_address')");
         $form_data = array();
         if (mysqli_query($db, $sql)) {
             $form_data['success'] = true;
@@ -19,7 +27,7 @@ include("connection.php");
 		} else {
             $form_data['success'] = false;
             $form_data['error_msg'] = $sql;
-        }
+        } */
 
         $db->close();
 
